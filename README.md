f6862adce361b135c0c949ae4283b786cf30f1e7
Dilo07 <andrea.dilorenzo07@gmail.com>
2022-12-06 16:57:05 +0100

    fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CrmCentroEstetico
+# CrmTemplate
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.10.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/crm-centro-estetico'),
+      dir: require('path').join(__dirname, './coverage/crm-template'),
       subdir: '.',
       reporters: [
         { type: 'html' },
