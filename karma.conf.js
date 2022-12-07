1f5c08b483ee3ce5f166ee2923df487591b7c09b
Dilo07 <andrea.dilorenzo07@gmail.com>
2022-12-07 10:32:12 +0100

    fix title

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/crm-template'),
+      dir: require('path').join(__dirname, './coverage/dema-template-gui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
