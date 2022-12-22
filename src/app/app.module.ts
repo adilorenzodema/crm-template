15899a748e48956ee5d17149e9dbe1452fa3f1e9
adilorenzodema <a.dilorenzo@dema-engineering.it>
2022-12-22 11:20:05 +0100

    add eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ExpansionComponent } from './components/expansion/expansion.component';
 import { HelpPageComponent } from './components/help-page/help-page.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { LoginComponent } from './core/login/login.component';
 import { MaterialModule } from './shared/modules/material.module';
 import { TemplateModule } from './template/template.module';
-import { LoginComponent } from './core/login/login.component';
-import { UserManagementComponent } from './components/user-management/user-management.component';
 
 @NgModule({
   declarations: [
