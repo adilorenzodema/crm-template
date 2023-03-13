37d2dd8747f1237940d26c0102dbe656a42a6ad3
adilorenzodema <a.dilorenzo@dema-engineering.it>
2023-03-13 14:55:01 +0100

    remove unusued

--- projects/dema-users-management/src/lib/domain/interface.ts
+++ projects/dema-users-management/src/lib/domain/interface.ts
@@ -2,15 +2,3 @@ export interface UserProfile {
   profileCode: string;
   profileName: string;
 }
-
-export type Code = 'delete-user' | 'edit-user' | 'insert-user' | 'view-user' | 'activate-user';
-
-export interface Operation {
-  code: Code;
-  description: string;
-  value: string;
-}
-export interface Permissions {
-  menuItemKey: string;
-  operations: Operation[];
-}
