39c8ceb616f7e74e18a1f0b856586a44a50f8934
gquaresima <g.quaresima@dema-engineering.it>
2023-03-17 09:57:05 +0100

    Cleaned filter file

--- projects/dema-movyon-template/src/lib/components/select-filter/select-filter.module.ts
+++ projects/dema-movyon-template/src/lib/components/select-filter/select-filter.module.ts
@@ -3,11 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SelectFilterComponent } from './select-filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LibMaterialModule } from '../../shared/module/material.module';
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 8cd98d4f10dde4c54d5d8dafa456199f5efc2d60
 
 
 @NgModule({
