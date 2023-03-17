8cd98d4f10dde4c54d5d8dafa456199f5efc2d60
adilorenzodema <a.dilorenzo@dema-engineering.it>
2023-03-17 09:49:11 +0100

    fix

--- projects/dema-movyon-template/src/lib/components/select-filter/select-filter.module.ts
+++ projects/dema-movyon-template/src/lib/components/select-filter/select-filter.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectFilterComponent } from './select-filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LibMaterialModule } from 'dist/dema-movyon-template';
-
+import { LibMaterialModule } from '../../shared/module/material.module';
 
 
 @NgModule({
